327e9230b1dde3446c2eafac8fd4b7bc949ee1f7
Donald Yeh <Donald.Yeh@gmail.com>
2019-02-21 17:30:58 -0500

    first commit

---
+++ word_bank.py
@@ -0,0 +1,20 @@
+import requests
+import random
+
+
+def word_bank():
+    word_list = []
+    word_site = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
+    response = requests.get(word_site)
+
+    WORDS = response.text.split()  # a list of words
+
+    for word in WORDS:
+        word_list.append(word)
+
+    converted_word = bytes(random.choice(word_list), 'utf-8')
+    return converted_word
+    # random.choice(word_list) #picks a random item from the list
+
+
+# print(word_bank())
